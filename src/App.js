d6dffe76dd09ba820cc97ea6d2d83c23964a2578
ninajavier <ninajavier13@gmail.com>
2023-03-27 20:42:42 -0400

    transferred assets and README file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Home from './components/pages/Home';
+import Cart from './components/pages/Cart';
+import CheckOut from './components/pages/CheckOut';
+import ExclusiveForm from './components/pages/ExclusiveForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+      <Cart />
+      <CheckOut />
+      <ExclusiveForm />
+      <p>Hello World</p>
     </div>
   );
 }
